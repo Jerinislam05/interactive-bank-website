ab9d41dcd82e6226c143ce2704f2f60d1353bbd0
Jerinislam05 <jerinislam05@gmail.com>
2023-11-17 10:34:28 +0000

    navigate to another page

--- login.js
+++ login.js
@@ -13,8 +13,8 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     //danger: do not verify email password on the client side
     // step 4: verify email password
     if (email === 'jerin@islam.com' && password === 'secret') {
-        console.log('valid user');
+        window.location.href = 'bank.html';
     } else {
-        console.log('invalid user');
+        alert('invalid password');
     }
 })
