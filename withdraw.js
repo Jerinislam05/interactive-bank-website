9c4ce8be68bd7aae1ac0a24a5ebe358e9bc53f2b
Jerinislam05 <jerinislam05@gmail.com>
2023-11-18 19:34:59 +0000

    completed withdraw file

---
+++ withdraw.js
@@ -0,0 +1,39 @@
+/* 
+1. add event handler with the withdraw button
+2. get the withdraw amount from the withdraw input field
+2.1. also make sure to convert the input into a number by using parseFloat
+3. get prev withdraw total
+4. calculate total withdraw amount
+4.1. set total withdraw amount 
+5. get the previous balance total
+6. calculate new balance total
+6.1. set new balance total 
+7. clear the withdraw field
+*/
+// step 1 :
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // step 2 :
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step 3 :
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const prevWithdrawTotalString = withdrawTotalElement.innerText;
+    const prevWithdrawTotal = parseFloat(prevWithdrawTotalString);
+
+    // step 4 :
+    const currentWithdrawTotal = prevWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 5 :
+    const balanceTotalElement = document.getElementById('balance-total');
+    const prevBalanceTotalString = balanceTotalElement.innerText;
+    const prevBalanceTotal = parseFloat(prevBalanceTotalString);
+    // step 6 :
+    const newBalanceTotal = prevBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    // step 7:
+    withdrawField.value = '';
+})
